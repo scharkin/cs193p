d94ffad0f2487b307430070e03834ffa06754f23
Sergey Charkin <scharkin@mobiquityinc.com>
2015-01-03 13:49:11 -0500

    create Matchismo

---
+++ assignment1/Matchismo/Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Sergey Charkin on 1/3/15.
+//  Copyright (c) 2015 Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ assignment1/Matchismo/Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Sergey Charkin on 1/3/15.
+//  Copyright (c) 2015 Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
